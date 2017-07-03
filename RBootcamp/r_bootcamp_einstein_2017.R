e4577f9692f9d4edd926b508c4b7adac745c49ff
mseinstein <protect3f@gmail.com>
2017-07-03 09:04:16 -0400

    added R file which the markdown implements

---
+++ RBootcamp/r_bootcamp_einstein_2017.R
@@ -0,0 +1,28 @@
+# SECTION 1 - THE BASICS
+
+# Mathemtical and Boolean Operations
+-sqrt(25) + (5+3)/4*7 - 2^2
+5%/%3 # Integer Division
+5%%3 # Modulo (remainder after division)
+5 == 6
+5 != 6
+83 > 25 >= 23
+83 > (25 >= 23)
+5 > 3 & 3 < 2
+5 > 3 | 3 < 2
+
+# Lists and Sequences
+c(5,3,2,1)
+seq(1,10,2)
+c(seq(1,10,2), 25, 10) > 12
+
+# Variables and Assignment
+x = 5+3
+(x = 5+3)
+x <- 5+3
+(x <- 5+3)
+y <- x
+x <- 5+3 > 2
+
+
+# SECTION 2 - <- 
